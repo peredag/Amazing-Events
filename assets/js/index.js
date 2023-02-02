af685a35e5d7bb3ad91f1bc7a8537cb07c31f589
Greta <greta.pereda@gmail.com>
2023-02-02 08:25:54 -0400

    task 2

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+const main = document.getElementById("row") 
+
+let eventosInfo = data.events 
+
+
+for (let eventoInfo of eventosInfo) { 
+
+    let divContenedor = document.createElement("div")
+
+     divContenedor.className = "card col-sm-12 col-md-4 col-lg-3 mb-1"  
+                                
+     divContenedor.innerHTML = `
+     
+            <img src="${eventoInfo.image}</h5>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${eventoInfo.name}</h5></h5>
+                <p class="card-text">${eventoInfo.description}</h5></p>
+                <div class="card-body">
+                <a href="./assets/detail.html" class="card-link">$${eventoInfo.price}</a>
+                <a href="./assets/detail.html" class="card-link">${eventoInfo.category}</a>
+                </div>
+            </div>
+        </div>
+     
+        `
+     main.appendChild(divContenedor)
+} 
+
+
+
